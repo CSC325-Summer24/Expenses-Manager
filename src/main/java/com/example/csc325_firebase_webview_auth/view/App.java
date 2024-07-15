d46f7ea696541f776f5798175adcea2d9f1d4b26
neomodulo <157737128+andrewkehoe@users.noreply.github.com>
2024-07-15 17:47:13 -0400

    rounded edges and social media icon

--- src/main/java/com/example/csc325_firebase_webview_auth/view/App.java
+++ src/main/java/com/example/csc325_firebase_webview_auth/view/App.java
@@ -31,7 +31,7 @@ public class App extends Application {
     public void start(Stage primaryStage) throws Exception {
         //fstore = contxtFirebase.firebase();
         //fauth = FirebaseAuth.getInstance();
-        scene = new Scene(loadFXML("/files/AccessFBView.fxml"),652,652);
+        scene = new Scene(loadFXML("/files/mainpage.fxml"),652,652);
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/files/main.css")).toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.show();
